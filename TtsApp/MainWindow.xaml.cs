15e6a70d06af088efe08aabd869da3ca5ae5c4bb
TartunovVA <Vitaliy.Tartunov@GMail.com>
2017-06-26 16:04:26 +0700

    Add very simple case to use internal windows TTS engine.

---
+++ TtsApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Speech.Synthesis;
+
+namespace TtsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly SpeechSynthesizer _reader;
+        const string voiceName = "Microsoft David Desktop";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _reader = new SpeechSynthesizer();
+
+            var voiceInstalled = _reader.GetInstalledVoices()
+                                     .FirstOrDefault(x => x.VoiceInfo.Description.Contains(voiceName)) != null;
+
+            if (!voiceInstalled)
+            {
+                MessageBox.Show($"Voice '{voiceName}' is not installed in system. Now we exit");
+            }
+
+            _reader.SelectVoice(voiceName);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _reader.Speak(TextToSpeakBox.Text);
+        }
+    }
+}
